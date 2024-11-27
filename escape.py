5ad34f33e38d4536817bbabbc89f80ab8c3a983d
akash8992 <akash8992@gmail.com>
2024-11-27 08:45:54 +0530

    some teaching

---
+++ escape.py
@@ -0,0 +1,8 @@
+text1 = "this is \\n 'python\' teaching"
+print(text1)
+
+
+# text2 = 'this is "python\" teaching'
+
+
+# print(text2)
---
+++ string.py
@@ -0,0 +1,12 @@
+# name = "AkashSingh"
+# name = 'AkashSingh'
+# # name = '''akash
+# # singh'''
+# print(name)
+
+# print(len(name))
+# length = len(name[4])
+# print(length)
+
+
+# print(name[0:3])
